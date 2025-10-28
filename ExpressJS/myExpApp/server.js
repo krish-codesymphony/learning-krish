f077cd677107832369657751e24f85572a4f47cd
Krish Pithadia <krish.codesymphony@gmail.com>
2025-10-28 11:10:05 +0530

    routes /users and /users:userId created for getting data

--- ExpressJS/myExpApp/server.js
+++ ExpressJS/myExpApp/server.js
@@ -4,13 +4,17 @@ import data from "./data.json" with {"type": "json"};
 const app = e();
 app.set("content-type", "application/json")
 
-app.get("/", (request, response) => {
+app.get("/users", (request, response) => {
     response.send(data);
 })
 
-// app.get("/{id}", (request, response) => {
-//     response.send(request.params);
-// })
+// /user/1
+app.get("/users/:userId", (request, response) => {
+    // response.send(request.params);
+    const found = data.find((elm) => elm.id == request.params.userId)
+    // console.log(found);
+    response.send(found);
+})
 
 app.listen(8081, () => {
     console.log("App Started at: http://localhost:8081");
