0245f1f89a498d06e0dca468164258d12ba82f58
Krish Pithadia <krish.codesymphony@gmail.com>
2025-10-28 18:30:06 +0530

    added error handling and condition based response

--- ExpressJS/myExpApp/server.js
+++ ExpressJS/myExpApp/server.js
@@ -2,19 +2,42 @@ import express from "express";
 import data from "./data.json" with {"type": "json"};
 
 const app = express();
-const PORT = 8081
-app.use(express.json())
+const PORT = 8081;
+app.use(express.json());
+app.use((err, req, res, next) => {
+    res.status(res.statusCode).send({
+        message: err.message
+    });
+});
 
 app.get("/users", (request, response) => {
     const pageCount = data.length / 10;
-    let page = Number(request.query.page) || 1;
-    if (page > pageCount) page = pageCount;
-    response.send(data.slice(page * 10 - 10, page * 10));
+    let pageNo = 0;
+    try {
+        pageNo = request.query.page;
+        if (pageNo == null || pageNo == undefined || pageNo == "")
+            throw new Error("Page number is not mentioned...!");
+        else if (pageNo > pageCount) {
+            response.status(400);
+            throw new Error("Page number Out of Bound...!");
+        }
+        else {
+            response.send(data.slice(pageNo * 10 - 10, pageNo * 10));
+        }
+    }
+    catch (e) {
+        console.log(e.message);
+        response.send(data);
+    }
 })
 
 app.get("/users/:userId", (request, response) => {
     const found = data.find((elm) => elm.id == request.params.userId)
-    response.send(found);
+    if (!found) {
+        response.status(400);
+        response.send("No User found with this ID...!");
+    } else
+        response.send(found);
 })
 
 app.listen(PORT, () => {
