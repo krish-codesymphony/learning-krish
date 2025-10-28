02e72105206fb828e88c48905debc572f52a46f3
Krish Pithadia <krish.codesymphony@gmail.com>
2025-10-28 12:12:35 +0530

    added to gitignore package-lock.json, updated server.js

--- ExpressJS/myExpApp/server.js
+++ ExpressJS/myExpApp/server.js
@@ -2,10 +2,19 @@ import e from "express";
 import data from "./data.json" with {"type": "json"};
 
 const app = e();
-app.set("content-type", "application/json")
+// app.set("content-type", "application/json")
+app.use(e.json())
 
 app.get("/users", (request, response) => {
-    response.send(data);
+    const pageCount = data.length / 10; // divide data to 10-10 grp
+    let page = Number(request.query.page) || 1; //set the page no.
+    if (page > pageCount) page = pageCount;
+
+    // say page=1 so--
+    // page*10-10 -> 1*10 -> 10 - 10 -> _0_,
+    // page*10 -> 1*10 -> _10_
+    // Therefore, starts with index _0_ and end at _9_(excluding the 10th)
+    response.send(data.slice(page * 10 - 10, page * 10));
 })
 
 // /user/1
