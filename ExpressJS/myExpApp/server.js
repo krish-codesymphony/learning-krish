10180f001338b8226b78d7d4bda106651a7aa638
Krish Pithadia <krish.codesymphony@gmail.com>
2025-10-28 15:42:08 +0530

    updated code to standards and added Array type to data.json

--- ExpressJS/myExpApp/server.js
+++ ExpressJS/myExpApp/server.js
@@ -1,30 +1,22 @@
-import e from "express";
+import express from "express";
 import data from "./data.json" with {"type": "json"};
 
-const app = e();
-// app.set("content-type", "application/json")
-app.use(e.json())
+const app = express();
+const PORT = 8081
+app.use(express.json())
 
 app.get("/users", (request, response) => {
-    const pageCount = data.length / 10; // divide data to 10-10 grp
-    let page = Number(request.query.page) || 1; //set the page no.
+    const pageCount = data.length / 10;
+    let page = Number(request.query.page) || 1;
     if (page > pageCount) page = pageCount;
-
-    // say page=1 so--
-    // page*10-10 -> 1*10 -> 10 - 10 -> _0_,
-    // page*10 -> 1*10 -> _10_
-    // Therefore, starts with index _0_ and end at _9_(excluding the 10th)
     response.send(data.slice(page * 10 - 10, page * 10));
 })
 
-// /user/1
 app.get("/users/:userId", (request, response) => {
-    // response.send(request.params);
     const found = data.find((elm) => elm.id == request.params.userId)
-    // console.log(found);
     response.send(found);
 })
 
-app.listen(8081, () => {
-    console.log("App Started at: http://localhost:8081");
-})
+app.listen(PORT, () => {
+    console.log(`App Started at: http://localhost:${PORT}`);
+});
