9de0581977ceae6c3a2377bd66ddb9a0d4e6e99d
Krish Pithadia <krish.codesymphony@gmail.com>
2025-10-29 17:52:59 +0530

    created find one api call using ejs view engine

--- ExpressJS/myExpApp/server.js
+++ ExpressJS/myExpApp/server.js
@@ -1,41 +1,40 @@
-import express from "express";
-import data from "./data.json" with {"type": "json"};
+const express = require("express");
+const path = require('path');
+const ejs = require("ejs");
+const data = require("./data.json");
 
 const app = express();
 const PORT = 8081;
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
-app.use((err, req, res, next) => {
-    res.status(res.statusCode).send({
-        message: err.message
-    });
+
+app.get("/", (req, res) => {
+    res.render("index", { data: JSON.stringify(data, null, 3), pageNo: 0 });
 });
 
 app.get("/users", (request, response) => {
-    const pageCount = data.length / 10;
-    let pageNo = 0;
     try {
-        pageNo = request.query.page;
-        if (pageNo == null || pageNo == undefined || pageNo == "")
-            throw new Error("Page number is not mentioned...!");
-        else if (pageNo > pageCount) {
-            response.status(400);
-            throw new Error("Page number Out of Bound...!");
-        }
-        else {
-            response.send(data.slice(pageNo * 10 - 10, pageNo * 10));
-        }
+        const pageCount = data.length / 10;
+        console.log(request.body);
+
+        let pageNo = request.body || 1;
+        const paged = data.slice(pageNo * 10 - 10, pageNo * 10);
+        response.send({
+            data: paged,
+            pageNo: pageNo
+        });
     }
     catch (e) {
         console.log(e.message);
-        response.send(data);
+        response.send(e.message);
     }
-})
+});
 
 app.get("/users/:userId", (request, response) => {
-    const found = data.find((elm) => elm.id == request.params.userId)
+    const found = data.find((elm) => elm.id == request.params.userId);
     if (!found) {
-        response.status(400);
-        response.send("No User found with this ID...!");
+        response.status(400).send("No User Found with This ID...!");
     } else
         response.send(found);
 })
